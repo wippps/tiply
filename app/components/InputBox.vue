a47594af53fe8bec8085fe966b669194b321a638
isma <isma@gmail.com>
2025-08-04 13:37:30 +0500

    components added

---
+++ app/components/InputBox.vue
@@ -0,0 +1,41 @@
+<script setup>
+const labels = ([
+    {text: "Счет"},
+    {text: "Имя"},
+    {text: "Псевдоним"},
+    {text: "Никнейм"},
+])
+
+</script>
+
+<template>
+    <div class="inputBox" v-for="label in labels" :key="label.id">
+        <label for="input" class="inputBox__label">{{ label.text }}</label>
+        <input type="text" id="input" class="inputBox__input">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.inputBox {
+    max-width: 630px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+    &__label {
+        font-size: 20px;
+        font-family: Montserrat;
+        font-weight: 700;
+        color: #42444D;
+    }
+
+
+    &__input {
+        padding: 20px;
+        outline: none;
+        border: 1px solid #00000033;
+        border-radius: 10px;
+    }
+}
+</style>
--- app/modules/author/components/SettingProfile.vue
+++ app/modules/author/components/SettingProfile.vue
@@ -23,9 +23,10 @@
                 Текст
             </h2>
             <Message 
-                placeholder="Введите сообщение"
-                maxLength="300"
+                placeholder="Тут можно написать о себе или поблагодарить пользователей за поддержку "
+                :maxLength="300"
                 mode="auth"
+                v-model="messageText"
             />
             <div class="profile__content-medias">
                 <h2 class="profile__content-title">
@@ -36,6 +37,29 @@
                 </p>
             </div>
             <LinkMedia />
+            <div class="profile__content-verification">
+                <div class="profile__content-image">
+                    <h2 class="profile__content-title">
+                        Подтверждение социальных сетей
+                    </h2>
+                    <img src="@/assets/images/components/verification.png" alt="" class="profile__content-img">
+                    <button class="profile__content-camera">
+                        <img src="@/assets/images/components/camera.png" alt="">
+                    </button>
+                </div>
+                <div class="profile__content-warning">
+                    <p class="profile__content-text">
+                        Для подтверждения владения вашими социальными сетями, загрузите скриншот, который демонстрирует, что одна из социальных сетей принадлежит вам.
+                    </p>
+                    <button class="profile__content-send">
+                        Отправить
+                    </button>
+                    <p class="profile__content-text">
+                        Убедитесь в верности написанной информации. После отправки на модерацию, вам придется подождать некотрое время, прежде чем снова вносить изменения. 
+                        Заявки рассматриваются в течение 24 часов.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -78,6 +102,59 @@
             font-size: 15px;
             color: #42444D99;
             margin-bottom: 35px;
+        }
+
+        &-verification{
+            display: flex;
+            flex-direction: column;
+            gap: 40px;
+            margin-top: 50px;
+            width: 100%;
+        }
+
+        &-warning{
+            display: flex;
+            flex-direction: column;
+            gap: 25px;
+        }
+
+        &-text{
+            font-weight: 300;
+            font-size: 15px;
+            color: #000000;
+        }
+
+        &-send{
+            width: 100%;
+            padding: 15px 0;
+            background: #42444D;
+            border-radius: 5px;
+
+            font-weight: 700;
+            font-size: 20px;
+            color: #ffffff;
+            border: none;
+            cursor: pointer;
+        }
+
+        &-image{
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            gap: 40px;
+        }
+
+        &-camera{
+            padding: 6px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #FF6D4D;
+            border-radius: 8px;
+            border: none;
+            position: absolute;
+            right: 0;
+            bottom: -19px;
         }
     }
 }
